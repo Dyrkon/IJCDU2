ee356858c39ac03b1a87082ff7a95553d993811f
dyrkon <dyrkon603@gmail.com>
2021-04-02 11:22:35 +0200

    mostly working, negative numbers need patch

--- tail.c
+++ tail.c
@@ -11,20 +11,105 @@
 
 #define MAX_LINES 511
 #define MINIMAL_N_OF_LINES 5
+#define ABS(x) ((x < 0) ? (x *= -1) : x)
 
 typedef struct input {
     char **lines;
-    size_t line_number;
-    size_t lines_allocated;
+    long line_number;
+    long lines_allocated;
 }input_t;
+
+void free_all(input_t *input);
+
+input_t get_input(char *filename, bool is_file);
+
+int read_from_stdin(input_t *input);
+
+int read_from_file(input_t *input, FILE *fp);
+
+int alloc_lines(bool do_realloc, input_t *input, size_t n);
+
+void do_tail(char *filename, char *to_print, bool is_file);
+
+void usage();
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2)
+    {
+        if (argv[1])
+
+        do_tail(argv[1], "|", true);
+
+    }
+    else if (argc == 3)
+    {
+        if (strcmp(argv[1], "-n") != 0)
+        {
+            printf("Wrong parameters given!\n");
+            usage();
+        }
+        else
+            do_tail(NULL, argv[2], false);
+    }
+    else if (argc == 4)
+    {
+        if (strcmp(argv[1], "-n") != 0)
+        {
+            printf("Wrong parameters given!\n");
+            usage();
+        }
+        else
+            do_tail(argv[3], argv[2], true);
+    }
+    else
+    {
+        printf("Too few parameters supplied!\n");
+        usage();
+        return 0;
+    }
+
+    return 0;
+}
 
 void usage()
 {
     printf(
-           "Usage:\n"
-           " tail YOUR-FILE\n"
-           " tail -n (+/-)NUMBER-OF-LINES-TO-PRINT YOUR-FILE\n"
-           " tail -n (+/-)NUMBER-OF-LINES-TO-PRINT < YOUR-FILE\n");
+            "Usage:\n"
+            " tail YOUR-FILE\n"
+            " tail -n (+/-)NUMBER-OF-LINES-TO-PRINT YOUR-FILE\n"
+            " tail -n (+/-)NUMBER-OF-LINES-TO-PRINT < YOUR-FILE\n");
+}
+
+void do_tail(char *filename, char *to_print, bool is_file)
+{
+    input_t input;
+    char *end;
+
+    long lines_to_print = 0;
+
+    if (to_print[0] == '|')
+        lines_to_print = 10;
+    else
+        lines_to_print = strtol(to_print,&end, 10);
+
+    input = get_input(!is_file ? "" : filename,is_file);
+
+    if (input.line_number > 0 && input.lines != NULL)
+    {
+        if (lines_to_print > input.line_number)
+            lines_to_print = input.line_number;
+    }
+    else
+    {
+        free_all(&input);
+        return;
+    }
+
+    for (long i = input.line_number - lines_to_print; i < input.line_number; i++)
+        printf("%s\n", input.lines[i]);
+
+    free_all(&input);
 }
 
 int alloc_lines(bool do_realloc, input_t *input, size_t n)
@@ -96,25 +181,41 @@ int read_from_file(input_t *input, FILE *fp)
     return 0;
 }
 
-// REDO
 int read_from_stdin(input_t *input)
 {
-    int end_char = 0;
-    while (end_char != EOF)
-    {
-        if ((input->lines[input->line_number] = calloc(MAX_LINES + 1, sizeof(char))) == NULL)
-        {
-            fprintf(stderr, "Unable to allocate enough memory!\n");
-            input->line_number *= -1;
-            return -1;
-        }
+    bool end_found = false;
 
-        scanf("%s",input->lines[input->line_number]);
-        input->line_number++;
-        end_char = getc(stdin);
-        if (end_char != '\n')
+    if (alloc_lines(false, input, MINIMAL_N_OF_LINES))
+        return 1;
+
+    while (!end_found)
+    {
+        if (input->lines_allocated == input->line_number)
         {
-            fprintf(stderr, "Unable to read the full line.\n");
+            if (input->line_number == input->lines_allocated) {
+                if (alloc_lines(true, input, MINIMAL_N_OF_LINES))
+                    return 1;
+            }
+        }
+        else
+        {
+            int end_char = 0;
+            if ((input->lines[input->line_number] = malloc(MAX_LINES+1 * sizeof(char))) == NULL) {
+                fprintf(stderr, "Unable to allocate enough memory!\n");
+                return 1;
+            }
+
+            fscanf(stdin,"%511[^\n]s",input->lines[input->line_number]);
+            input->line_number++;
+            end_char = fgetc(stdin);
+            if (end_char == EOF)
+                end_found = true;
+            else if (end_char != '\n')
+            {
+                fscanf(stdin, "%*[^\n]");
+                end_char = fgetc(stdin);
+                fprintf(stderr, "Unable to read the full line.\n");
+            }
         }
     }
     return 0;
@@ -132,7 +233,6 @@ input_t get_input(char *filename, bool is_file)
 
         if ((fp = fopen(filename, "r")) == NULL) {
             fprintf(stderr, "Unable to open file!\n");
-            fclose(fp);
             input.lines = NULL;
             return input;
         }
@@ -141,7 +241,7 @@ input_t get_input(char *filename, bool is_file)
         {
             fprintf(stderr, "Unable to allocate enough memory!\n");
             fclose(fp);
-            input.line_number *= -1;
+            input.lines_allocated *= -1;
             return input;
         }
 
@@ -153,82 +253,29 @@ input_t get_input(char *filename, bool is_file)
         if (read_from_stdin(&input))
         {
             fprintf(stderr, "Unable to allocate enough memory!\n");
-            input.line_number *= -1;
+            input.lines_allocated *= -1;
             return input;
         }
     }
     return input;
 }
 
-int main(int argc, char *argv[])
+void free_all(input_t *input)
 {
-    input_t input;
-    if (argc == 2)
+    if (input->lines_allocated < 0)
     {
-        const char *filename = argv[1];
-    }
-    else if (argc == 3)
-    {
-        if (strcmp(argv[1], "-n"))
+        for (long i = 0; i < input->lines_allocated * (-1); i++)
         {
-            printf("Wrong parameters given!\n");
-            usage();
+            free(input->lines[i]);
         }
-        else
-        {
-            char *end;
-            long lines_to_print = strtol(argv[2],&end, 10);
-            input = get_input("",false);
-            if (input.lines_allocated < 0)
-            {
-                for (size_t i = 0; i < input.lines_allocated*(-1); i++)
-                {
-                    free(input.lines[i]);
-                }
-            }
-        }
-    }
-    else if (argc == 4)
-    {
-        if (strcmp(argv[1], "-n"))
-        {
-            printf("Wrong parameters given!\n");
-            usage();
-        }
-        else
-        {
-            char *end;
-            long lines_to_print = strtol(argv[2],&end, 10);
-            input = get_input(argv[3],true);
-
-            // input.line_number - lines_to_print
-            for (size_t i = input.line_number - lines_to_print; i < input.line_number; i++)
-            {
-                printf("%s\n", input.lines[i]);
-            }
-
-            if (input.line_number < 0)
-            {
-                for (size_t i = 0; i < input.lines_allocated * (-1); i++)
-                {
-                    free(input.lines[i]);
-                }
-            }
-            else
-            {
-                for (size_t i = 0; i < input.lines_allocated; i++)
-                {
-                    free(input.lines[i]);
-                }
-            }
-        }
+        free(input->lines);
     }
     else
     {
-        printf("Too few parameters supplied!\n");
-        usage();
-        return 0;
+        for (long i = 0; i < input->lines_allocated; i++)
+        {
+            free(input->lines[i]);
+        }
+        free(input->lines);
     }
-
-    return 0;
 }
